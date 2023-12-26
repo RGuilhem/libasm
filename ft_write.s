c4690419ea233ad81ff7b8b5fffad9c08ebdf4cf
Guilhem Raux <g.raux@icloud.com>
2023-12-26 15:05:00 +0100

    basically just calling write with same args

--- ft_write.s
+++ ft_write.s
@@ -1,3 +1,6 @@
 global ft_write
 
 ft_write:
+	mov rax, 1
+	syscall
+	ret
--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 size_t	ft_strlen(char *s);
 char	*ft_strcpy(char *dest, char *src);
 int	ft_strcmp(char *s1, char *s2);
+int	ft_write(int, char *, size_t);
 
 int main(int argc, char **argv) {
 	if (argc == 3)
@@ -17,6 +18,8 @@ int main(int argc, char **argv) {
 		printf("dest: %s\n", dest);
 		printf("ft_strcmp: %d\n", ft_strcmp(argv[1], argv[2]));
 		printf("strcmp: %d\n", strcmp(argv[1], argv[2]));
+		ft_write(1, argv[1], ft_strlen(argv[1]));
+		printf("\n");
 	}
 	else
 		printf("Usage: ./test_main <string1> <string2>");
