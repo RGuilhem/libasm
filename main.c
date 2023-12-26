8966ade45388dbf1963829631e44b97be23bec44
Guilhem Raux <g.raux@icloud.com>
2023-12-26 10:48:55 +0100

    test main now takes two string args

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 
 size_t ft_strlen(char *s);
 
-int main(void) {
-	char *tab = "Hello World";
-	int val = ft_strlen(tab);
-
-	printf("len: %d\n", val);
+int main(int argc, char **argv) {
+	if (argc == 3)
+	{
+		printf("argv[1]: %s\n", argv[1]);
+		printf("argv[2]: %s\n", argv[2]);
+	}
+	else
+		printf("Usage: ./test_main <string1> <string2>");
 	return (0);
 }
